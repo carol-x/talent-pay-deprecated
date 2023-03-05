f5f07e53f39c38c35754bd67410d5d6046808c7c
carolx <carolx@berkeley.edu>
2023-03-04 22:17:18 -0500

    finished payroll contract

--- contracts/Payroll.sol
+++ contracts/Payroll.sol
@@ -4,7 +4,6 @@ pragma solidity ^0.8.9;
 contract Payroll {
     uint public payPeriod;
     address payable public owner;
-    uint public totalAmount; 
     mapping(address => uint) public latestTime; 
     mapping(address => uint) public eachPayAmount;
     mapping(address => uint) public paymentCount;
@@ -15,41 +14,36 @@ contract Payroll {
 
     constructor(uint _payPeriod) payable {
         require(
-            _payPeriod > 1,
-            "Pay period cannot be too small "
+            _payPeriod > 0,
+            "Pay period cannot be less than a day!"
         );
         payPeriod = _payPeriod * 1 days;
         owner = payable(msg.sender);
     }
 
-    receive() external payable {
-        totalAmount += msg.value; 
-    }
+    receive() external payable {}
 
-    function depositPay(uint _amount, uint _interval) external payable {
-        require(_amount > 0, "Payment has to be greater than 0!"); 
-        require(msg.sender.balance > _amount, "You don't have enough ETH to pay");
+    function depositPay(uint _interval) public payable {
+        require(msg.value > 0, "Payment has to be greater than 0!"); 
+        require(msg.sender.balance > msg.value, "You don't have enough ETH to pay");
 
-        payable(this).transfer(_amount); 
-        emit DepositPay(_amount, msg.sender, block.timestamp);
+        payable(this).transfer(msg.value); 
+        emit DepositPay(msg.value, msg.sender, block.timestamp);
 
         latestTime[msg.sender] = block.timestamp;
-        eachPayAmount[msg.sender] = _amount / _interval; 
+        eachPayAmount[msg.sender] = msg.value / _interval; 
         paymentCount[msg.sender] = _interval; 
     }
 
-    function getPaid() public {
+    function getPaid(address commissioner) public {
         require(msg.sender == owner, "You are not the owner of the smart contract!");
-        for(uint i = 0; i <= commissioners.length; i++){
-            address commissioner = commissioners[i]; 
-            if (paymentCount[commissioner] > 0) {
-                if (block.timestamp >= payPeriod + latestTime[commissioner]) {
-                    latestTime[commissioner] = block.timestamp; // update last payment time
-                    paymentCount[msg.sender] -= 1; 
-                    owner.transfer(eachPayAmount[commissioner]); 
-                    emit GetPaid(address(this).balance, commissioner, block.timestamp);
-                }
-            }
+        if (paymentCount[commissioner] > 0) {
+            
+            latestTime[commissioner] = block.timestamp; // update last payment time
+            paymentCount[msg.sender] -= 1; 
+            owner.transfer(eachPayAmount[commissioner]); 
+            emit GetPaid(address(this).balance, commissioner, block.timestamp);
+            
         }
     }
 
